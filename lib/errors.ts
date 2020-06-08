6b9316797df13702b6b8f038f9ac90edc48c80ca
Daniel Kuliński <torozniks@gmail.com>
2020-06-08 14:39:14 +0200

    error lib

---
+++ lib/errors.ts
@@ -0,0 +1,16 @@
+import logger from './logger'
+
+export class KafkaError extends Error {
+  message: string
+  code: string
+
+  constructor(message: string, code: string) {
+    logger.error(message)
+    super(message)
+    this.code = code
+  }
+}
+
+export const KafkaErrorCodes = {
+  ALREADY_INSTANTIATED: 'KAF-1'
+}
